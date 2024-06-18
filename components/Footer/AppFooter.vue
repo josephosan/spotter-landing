cfa927bc43a450280fc23626edd5c633e756f99f
joseph.o <josephosan1381@gmail.com>
2024-06-18 16:32:06 +0330

    initialized footer

---
+++ components/Footer/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="app-footer">
+        footer
+    </div>
+</template>
+
+<style lang="scss">
+@import '/assets/scss/components/footer/app-footer.scss';
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,6 @@
 <template>
     <AppNavbar />
     <NuxtPage />
+
+    <AppFooter />
 </template>
