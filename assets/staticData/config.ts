36bc5ad8dff5302af2b47400edebc4504e561ef1
Joseph <joseph@Josephs-MacBook-Air.local>
2024-06-25 23:07:27 +0330

    added sidebare on phone screens '

--- assets/staticData/config.ts
+++ assets/staticData/config.ts
@@ -1,57 +1,59 @@
 export default {
-    auth: {},
-    default: {
-        nav: {
-            buttons: {
-                primary: {
-                    title: 'ورود',
-                    route: ''
-                },
-                normal: {
-                    title: 'ثبت نام',
-                    route: ''
-                }
-            },
-            pages: [
-                {
-                    title: 'محصولات',
-                    route: 'products'
-                },
-                {
-                    title: 'تدریس',
-                    route: 'teach'
-                },
-                {
-                    title: 'منابع',
-                    route: 'resources'
-                }
-            ]
+  auth: {},
+  default: {
+    nav: {
+      buttons: {
+        primary: {
+          title: 'ورود',
+          route: ''
         },
-        body: {
-            preview: {
-                title: 'دانش برای همه، در هر کجا!صرف نظر از مکان .',
-                subTitle: 'با استفاده از پلتفرم ما، می‌توانید به صورت رایگان به طیف گسترده‌ای از دوره‌های آموزشی دسترسی پیدا کنید و دانش خود را در زمینه‌های مختلف ارتقا دهید.',primaryButton: 'شروع کنید',
-                normalButton: 'اطلاعات بیشتر',
-                numbers: {
-                    field1: {
-                        number: 100000,
-                        title: 'دانش آموزان'
-                    },
-                    field2: {
-                        number: 1000,
-                        title: 'افراد دخیل'
-                    },
-                    field3: {  
-                        number: 100,
-                        title: 'دانش آموزان'
-                    }
-                },
-
-                image: {
-                    foreground: '/images/student.png',
-                    background: '/images/student-background.svg'
-                }
-            }
+        normal: {
+          title: 'ثبت نام',
+          route: ''
         }
+      },
+      pages: [
+        {
+          title: 'محصولات',
+          route: 'products'
+        },
+        {
+          title: 'تدریس',
+          route: 'teach'
+        },
+        {
+          title: 'منابع',
+          route: 'resources'
+        }
+      ]
+    },
+    body: {
+      preview: {
+        title: 'دانش برای همه، در هر کجا!صرف نظر از مکان .',
+        subTitle:
+          'با استفاده از پلتفرم ما، می‌توانید به صورت رایگان به طیف گسترده‌ای از دوره‌های آموزشی دسترسی پیدا کنید و دانش خود را در زمینه‌های مختلف ارتقا دهید.',
+        primaryButton: 'شروع کنید',
+        normalButton: 'اطلاعات بیشتر',
+        numbers: {
+          field1: {
+            number: 100000,
+            title: 'دانش آموزان'
+          },
+          field2: {
+            number: 1000,
+            title: 'افراد دخیل'
+          },
+          field3: {
+            number: 100,
+            title: 'دانش آموزان'
+          }
+        },
+
+        image: {
+          foreground: '/images/student.png',
+          background: '/images/student-background.svg'
+        }
+      }
     }
+  }
 }
--- components/Sidebar/PhoneSidebar.vue
+++ components/Sidebar/PhoneSidebar.vue
@@ -1,16 +1,40 @@
 <template>
+  <div class="phone-sidebar">
     <el-dialog v-model="phoneSidebarOpen" :fullscreen="true" :show-close="false">
-        <template #header>
-            <el-icon size="large" @click="phoneSidebarOpen = false">
-                <Close />
-            </el-icon>
-        </template>
+      <template #header>
+        <el-icon size="large" @click="phoneSidebarOpen = false">
+          <Close />
+        </el-icon>
+      </template>
+
+      <div class="body">
+        <div class="buttons">
+          <el-button>{{ navButtons.normal.title }}</el-button>
+          <el-button type="primary">{{ navButtons.primary.title }}</el-button>
+        </div>
+
+        <div class="menu">
+          <el-menu default-active="1" class="el-menu-vertical-demo">
+            <el-menu-item v-for="(page, index) in navPages" :index="index + ''">
+              <span>{{ page.title }}</span>
+            </el-menu-item>
+          </el-menu>
+        </div>
+      </div>
     </el-dialog>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { useAppStore } from '~/store/app';
+import { useAppStore } from '~/store/app'
 import { Close } from '@element-plus/icons-vue'
+import config from '~~/assets/staticData/config'
 
 const { phoneSidebarOpen } = storeToRefs(useAppStore())
+const navButtons = config.default.nav.buttons
+const navPages = config.default.nav.pages
 </script>
+
+<style lang="scss">
+@import '/assets/scss/components//sidebar/phone-sidebar.scss';
+</style>
